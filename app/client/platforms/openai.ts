e79c8fa8205c2ba21c5449213538d0d8d4766078
Bob Zeng <bobz@ciandt.com>
2024-03-18 15:24:52 +0800

    Add max_tokens paramater for vision model

--- app/client/platforms/openai.ts
+++ app/client/platforms/openai.ts
@@ -109,6 +109,9 @@ export class ChatGPTApi implements LLMApi {
       // max_tokens: Math.max(modelConfig.max_tokens, 1024),
       // Please do not ask me why not send max_tokens, no reason, this param is just shit, I dont want to explain anymore.
     };
+    if (visionModel) {
+      requestPayload.max_tokens = 300;
+    }
 
     console.log("[Request] openai payload: ", requestPayload);
 
